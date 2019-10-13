cbb5482c50ba7c59f0309b985f5a0bf44add2ef8
Sashank Gondala <sgondala2@gmail.com>
2019-10-13 00:04:37 -0400

    Revert "Finished HW2, Started HW3"
    This reverts commit 11b2ff22a5c1077bc87bcac904fc3eacf47a87df.

--- HW2/assignment/collect_submission.sh
+++ HW2/assignment/collect_submission.sh
@@ -1,3 +1,2 @@
 rm -f cs7643_hw2.zip
-jupyter-nbconvert --to pdf "*.ipynb"
-zip -r cs7643_hw2.zip . -x "*.ipynb_checkpoints*" "*README.md" "*collect_submission.sh" "*.pdf" ".env/*" "*.pyc" "__pycache__/*"
+zip -r cs7643_hw2.zip . -x "*.ipynb_checkpoints*" "*README.md" "*collect_submission.sh" ".env/*" "*.pyc" "__pycache__/*"
